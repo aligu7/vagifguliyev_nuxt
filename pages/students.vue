84b9b3b056c99fc1f50b47ae0fb7a44591c3733f
Ali-Guliyev <ali.guliyevoo7@gmail.com>
2024-12-23 22:51:34 +0300

    chore: updated students.vue and md

--- pages/students.vue
+++ pages/students.vue
@@ -1,8 +1,31 @@
+<script setup>
+const { data } = await useAsyncData("students", () => queryContent("/students").findOne());
+</script>
+
 <template>
   <div id="tooplate_main">
     <div id="students" class="main_box">
       <h1>PhD and Doctorate Students</h1>
-      <ContentDoc />
+
+      <a class="title" href="#_20-phd-students"><em>20 Ph.D. students</em></a>
+
+      <ol>
+        <li v-for="student in data.students.phd" :key="student.name">
+          <span>{{ student.name }}</span>
+          <span>{{ student.content }}</span>
+          <span>{{ student.date }}</span>
+        </li>
+      </ol>
+
+      <a class="title" href="#_20-phd-students"><em>5 Doctorate students</em></a>
+
+      <ol>
+        <li v-for="student in data.students.doctorate" :key="student.name">
+          <span>{{ student.name }}</span>
+          <span>{{ student.content }}</span>
+          <span>{{ student.date }}</span>
+        </li>
+      </ol>
 
       <div class="cleaner"></div>
     </div>
@@ -10,7 +33,15 @@
 </template>
 
 <style scoped>
-span {
-  color: white;
+ol li {
+  margin-bottom: 15px;
+}
+
+ol li span {
+  display: block;
+}
+
+a.title {
+  font-size: 20px;
 }
 </style>
